469c0ef31edb2c5599406042c6f5c0a9a51d00e2
William Irwin-Harris <w.irwinharris@gmail.com>
2015-12-30 16:16:23 +1300

    Fix parsing issues in the .obj file compiler
    Certain types of .obj files weren't being parsed correctly by the
    ObjCompiler class.
    
    Fix this by replacing the java.util.Scanner-based parsing with a new
    ObjCompiler.Lexer class, which is tailored to tokenizing .obj files.

--- src/ObjCompiler.java
+++ src/ObjCompiler.java
@@ -1,6 +1,5 @@
 import java.util.*;
 import java.util.Map.Entry;
-import java.util.regex.Pattern;
 import java.io.*;
 
 public class ObjCompiler implements AssetCompiler {
@@ -27,6 +26,160 @@ public class ObjCompiler implements AssetCompiler {
         }
     }
 
+    static class Lexer implements Closeable {
+        static enum TokenType {
+            STRING,
+            INTEGER,
+            FLOAT,
+            SYMBOL,
+        };
+
+        private BufferedReader reader;
+        private StringBuilder stringBuilder;
+        private TokenType tokenType;
+        private String currentToken;
+        private int c;
+
+        private void parseString() throws IOException {
+            tokenType = TokenType.STRING;
+            do {
+                stringBuilder.append((char)c);
+                c = reader.read();
+            } while (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'));
+        }
+
+        private void parseNumber(boolean isNegative) throws IOException {
+            if (isNegative) {
+                stringBuilder.append('-');
+            }
+            do {
+                stringBuilder.append((char)c);
+                c = reader.read();
+            } while ('0' <= c && c <= '9');
+            if (c == '.') {
+                tokenType = TokenType.FLOAT;
+                do {
+                    stringBuilder.append((char)c);
+                    c = reader.read();
+                } while ('0' <= c && c <= '9');
+            } else {
+                tokenType = TokenType.INTEGER;
+            }
+        }
+
+        private void parseSymbol() throws IOException {
+            tokenType = TokenType.SYMBOL;
+            stringBuilder.append((char)c);
+            c = reader.read();
+        }
+
+        private void skipWhitespace() throws IOException {
+            while (c != -1 && Character.isWhitespace(c)) {
+                c = reader.read();
+            }
+        }
+
+        private void skipCommentsAndWhitespace() throws IOException {
+            skipWhitespace();
+            while (c == '#') {
+                while (c != -1 && c != '\n') {
+                    c = reader.read();
+                }
+                skipWhitespace();
+            }
+        }
+
+        private void fetchNextToken() throws IOException {
+            skipCommentsAndWhitespace();
+            if (c == -1) { // end of file
+                currentToken = null;
+                tokenType = null;
+                return;
+            }
+            if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')) {
+                parseString();
+            }
+            else if ('0' <= c && c <= '9') {
+                parseNumber(false);
+            }
+            else if (c == '-') {
+                c = reader.read();
+                if ('0' <= c && c <= '9') {
+                    parseNumber(true);
+                } else {
+                    // Don't want to use parseSymbol() here as we've already read the next character.
+                    tokenType = TokenType.SYMBOL;
+                    stringBuilder.append('-');
+                }
+            }
+            else {
+                parseSymbol();
+            }
+            currentToken = stringBuilder.toString();
+            stringBuilder.setLength(0);
+        }
+
+        public Lexer(File file) throws IOException {
+            reader = new BufferedReader(new FileReader(file));
+            stringBuilder = new StringBuilder();
+            c = reader.read();
+            fetchNextToken();
+        }
+
+        public boolean hasNext() {
+            return currentToken != null;
+        }
+
+        public String nextToken() throws IOException {
+            String s = currentToken;
+            fetchNextToken();
+            return s;
+        }
+
+        public int nextInt() throws IOException {
+            if (tokenType != TokenType.INTEGER) {
+                throw new InputMismatchException();
+            }
+            int i = Integer.parseInt(currentToken);
+            fetchNextToken();
+            return i;
+        }
+
+        public float nextFloat() throws IOException {
+            if (tokenType != TokenType.FLOAT) {
+                throw new InputMismatchException();
+            }
+            float f = Float.parseFloat(currentToken);
+            fetchNextToken();
+            return f;
+        }
+
+        public String nextSymbol() throws IOException {
+            if (tokenType != TokenType.SYMBOL) {
+                throw new InputMismatchException();
+            }
+            String s = currentToken;
+            fetchNextToken();
+            return s;
+        }
+
+        public boolean hasNext(String s) {
+            return currentToken.equals(s);
+        }
+
+        public void skipLine() throws IOException {
+            while (c != '\n') {
+                c = reader.read();
+            }
+            fetchNextToken();
+        }
+
+        @Override
+        public void close() throws IOException {
+            reader.close();
+        }
+    };
+
     @Override
     public boolean compile(File inputFile, File outputFile) {
         List<Vector3> positions = new ArrayList<Vector3>();
@@ -34,42 +187,40 @@ public class ObjCompiler implements AssetCompiler {
         Map<Vertex, Integer> vertices = new HashMap<Vertex, Integer>();
         List<Integer> indices = new ArrayList<Integer>();
 
-        try (Scanner scanner = new Scanner(inputFile)) {
-            while (scanner.hasNext()) {
-                if (scanner.hasNext("#")) {
-                    scanner.nextLine(); // skip comment
-                }
-                else if (scanner.hasNext("v")) {
-                    scanner.next();
+        try (Lexer lexer = new Lexer(inputFile)) {
+            while (lexer.hasNext()) {
+                String token = lexer.nextToken();
+                if (token.equals("v")) {
                     Vector3 v = new Vector3();
-                    v.x = scanner.nextFloat();
-                    v.y = scanner.nextFloat();
-                    v.z = scanner.nextFloat();
+                    v.x = lexer.nextFloat();
+                    v.y = lexer.nextFloat();
+                    v.z = lexer.nextFloat();
                     positions.add(v);
-                    scanner.nextLine();
                 }
-                else if (scanner.hasNext("vn")) {
-                    scanner.next();
+                else if (token.equals("vn")) {
                     Vector3 v = new Vector3();
-                    v.x = scanner.nextFloat();
-                    v.y = scanner.nextFloat();
-                    v.z = scanner.nextFloat();
+                    v.x = lexer.nextFloat();
+                    v.y = lexer.nextFloat();
+                    v.z = lexer.nextFloat();
                     normals.add(v);
                 }
-                else if (scanner.hasNext("f")) {
-                    scanner.next();
+                else if (token.equals("f")) {
                     for (int i = 0; i < 3; ++i) {
-                        Pattern delim = scanner.delimiter();
-                        scanner.useDelimiter("(\\p{javaWhitespace}+)|(/)");
-                        int posIdx = scanner.nextInt();
+                        int posIdx = lexer.nextInt();
                         int normalIdx = Integer.MAX_VALUE;
-                        if (scanner.hasNextInt()) {
-                            normalIdx = scanner.nextInt();
-                            if (scanner.hasNextInt()) {
-                                normalIdx = scanner.nextInt();
+                        if (lexer.hasNext("/")) {
+                            lexer.nextToken();
+                            if (lexer.hasNext("/")) {
+                                lexer.nextToken();
+                                normalIdx = lexer.nextInt();
+                            } else {
+                                lexer.nextInt();
+                                if (lexer.hasNext("/")) {
+                                    lexer.nextToken();
+                                    normalIdx = lexer.nextInt();
+                                }
                             }
                         }
-                        scanner.useDelimiter(delim);
                         if (normalIdx == Integer.MAX_VALUE) {
                             System.out.println("Error: obj file doesn't have normals");
                             return false;
@@ -85,10 +236,10 @@ public class ObjCompiler implements AssetCompiler {
                         indices.add(index);
                     }
                 } else {
-                    scanner.nextLine();
+                    lexer.skipLine();
                 }
             }
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             return false;
         }
 
